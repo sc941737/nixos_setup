44995e876c3209d1ff4839b06125a08676fbf5ba
Serge <sc9500@proton.me>
2023-12-26 02:12:41 +0000

    Setup Home Manager

--- flake.nix
+++ flake.nix
@@ -2,16 +2,25 @@
   description = "Main";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }: 
   let 
     lib = nixpkgs.lib;
     system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = system;
+        inherit system;
         modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      d = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
   };
