b349d9456df73f5fd63a7bc6cd345b79198a9836
sc941737 <sergiusz.cichosz@proton.me>
2024-03-16 15:27:03 +0000

    Add common fonts

--- configuration.nix
+++ configuration.nix
@@ -110,6 +110,12 @@ in
 
   # Font config
   fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
     (nerdfonts.override { fonts = [ "SourceCodePro" "Hack" ]; })
   ];
 
